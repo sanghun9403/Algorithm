77b5c7655b982c9b98a55148ee197a064a52bc64
sanghun9403 <132434240+sanghun9403@users.noreply.github.com>
2023-06-02 15:56:14 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -10,4 +10,6 @@ function solution(price) {
         answer = price;
     }
     return answer;
+    
+    // price >= 500000 ? price = Math.floor(price * 0.8) : (price >= 300000 ? price = Math.floor(price * 0.9) : (price >= 100000 ? price = Math.floor(price * 0.95) : price))
 }
