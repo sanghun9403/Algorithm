b41ef259456ea6031bdb540faec20d1dc274d80f
sanghun9403 <132434240+sanghun9403@users.noreply.github.com>
2023-06-02 16:41:02 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,7 +1,11 @@
 function solution(sides) {
-    let answer = 0;
-    sides.sort((a, b) => a - b);
-    let max = sides.pop();
-    console.log(max)
-    return max >= sides[0] + sides[1] ? answer = 2 : answer = 1;
+    // let answer = 0;
+    // sides.sort((a, b) => a - b);
+    // let max = sides.pop();
+    // console.log(max)
+    // return max >= sides[0] + sides[1] ? answer = 2 : answer = 1;
+    let max = Math.max(...sides)
+ 
+    let el = sides.reduce((acc, cur) => acc + cur, 0);
+    return max >= (el - max) ? 2 : 1
 }
