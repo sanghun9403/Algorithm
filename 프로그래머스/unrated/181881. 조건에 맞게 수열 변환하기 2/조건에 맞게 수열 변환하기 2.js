74680a12c5766e407a3590cd5d8d095e7c2ff591
sanghun9403 <132434240+sanghun9403@users.noreply.github.com>
2023-06-12 23:02:14 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -21,4 +21,14 @@ function solution(arr) {
     }
 
     return cnt;
+    
+    // while(!arr.every(x => (x >= 50 && x % 2 === 1) || (x < 50 && x % 2 === 0))) {
+    // arr = arr.map(x => {
+    //     if (x >= 50 && x % 2 === 0) return x / 2;
+    //     if (x < 50 && x % 2 === 1) return x * 2 + 1;
+    //     return x;
+    // })
+    // n++;
+    // }
+    // return n;
 }
