64432fe73b6109332dbf2d6040adcd9c85acb43d
sanghun9403 <132434240+sanghun9403@users.noreply.github.com>
2023-06-12 22:16:00 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,14 +1,21 @@
 function solution(numbers) {
-    let obj = {
-        zero : 0, one : 1, two : 2, three : 3, four : 4,
-        five : 5, six : 6, seven : 7, eight : 8, nine : 9
-    };
+//     let obj = {
+//         zero : 0, one : 1, two : 2, three : 3, four : 4,
+//         five : 5, six : 6, seven : 7, eight : 8, nine : 9
+//     };
     
     
-    let num = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (val) => {
-        return obj[val]
-    })
+//     let num = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (val) => {
+//         return obj[val]
+//     })
     
     
-    return Number(num);
+//     return Number(num);
+    
+    let numStr = [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ];
+
+    numStr.forEach((str, idx) => {
+        numbers = numbers.replaceAll(str, idx);
+    });
+    return Number(numbers);
 }
