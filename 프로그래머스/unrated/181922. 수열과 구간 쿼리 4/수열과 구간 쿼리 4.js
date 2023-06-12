1dcc70a81ec366d1a4ef6250bff1abe7c5cf22ac
sanghun9403 <132434240+sanghun9403@users.noreply.github.com>
2023-06-12 21:56:42 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 4.67 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -1,8 +1,15 @@
 function solution(arr, queries) {
-    queries.forEach(([a, b, c]) => {
-        for(i = a; i <= b; i++) {
-            if(i % c === 0) arr[i]++
-        }
-    })
-    return arr
+//     queries.forEach(([a, b, c]) => {
+//         for(i = a; i <= b; i++) {
+//             if(i % c === 0) arr[i]++
+//         }
+//     })
+//     return arr
+    
+    for(let [s, e, k] of queries) {
+    for(let i = s; i <= e; i++) {
+        if(i % k === 0) arr[i]++;
+    }
+    }
+    return arr;
 }
