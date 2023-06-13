4b936d130dc2e8beb7a093957e146b3695790786
sanghun9403 <132434240+sanghun9403@users.noreply.github.com>
2023-06-13 22:04:46 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -8,4 +8,9 @@ function solution(my_string) {
         }
     })
     return answer;
+    
+    // let ans = Array(58).fill(0);
+    // for (let c of my_string) ans[c.charCodeAt()-65]++;
+    // ans.splice(26,6);
+    // return ans
 }
