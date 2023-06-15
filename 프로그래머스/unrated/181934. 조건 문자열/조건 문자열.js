4dd321c62ed7f2bccc3d2de61c9a4d1a515a6c58
sanghun9403 <132434240+sanghun9403@users.noreply.github.com>
2023-06-15 23:28:03 +0900

    [unrated] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -1,12 +1,7 @@
 function solution(ineq, eq, n, m) {
     let answer = 0
-    if (n === m && eq === "=") {
-        return 1
-    } else if (n < m && ineq === "<") {
-        return 1
-    } else if (n > m && ineq === ">") {
-        return 1
-    } else {
-        return 0
-    }
+    if (n === m && eq === "=") return 1
+    if (n < m && ineq === "<") return 1
+    if (n > m && ineq === ">") return 1
+    return 0
 }
