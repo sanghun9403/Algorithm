fabcce306c9b88d63d974e1793adb59c839a015d
sanghun9403 <132434240+sanghun9403@users.noreply.github.com>
2023-06-15 22:09:23 +0900

    [unrated] Title: 두 수의 합, Time: 38.26 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181846. 두 수의 합/두 수의 합.js
+++ 프로그래머스/unrated/181846. 두 수의 합/두 수의 합.js
@@ -1,4 +1,22 @@
 function solution(a, b) {
     let answer = BigInt(a) + BigInt(b)
     return answer.toString()
+    
+//     var answer = '';
+//     const length = Math.max(a.length, b.length);
+//     a = "0".repeat(length - a.length) + a;
+//     b = "0".repeat(length - b.length) + b;
+
+//     let add = false;
+
+//     for(let i = length - 1; i >= 0; i--) {
+//         let num = parseInt(a[i]) + parseInt(b[i]) + (add ? 1 : 0);
+//         add = num >= 10;
+//         if(add) num -= 10;
+//         answer = num + answer;
+//     }
+
+//     if(add) answer = "1" + answer;
+
+//     return answer;
 }
