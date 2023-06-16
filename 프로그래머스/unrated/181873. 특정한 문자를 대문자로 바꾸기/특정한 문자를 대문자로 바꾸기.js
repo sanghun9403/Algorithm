660b468c19fd12615d8a892681d893e5f4b04afa
sanghun9403 <132434240+sanghun9403@users.noreply.github.com>
2023-06-16 20:03:52 +0900

    [unrated] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.04 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
+++ 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -1,5 +1,6 @@
 function solution(my_string, alp) {
-    let capital = alp.toUpperCase();
-    let answer = my_string.replaceAll(alp, capital)
-    return answer
+    // let capital = alp.toUpperCase();
+    // let answer = my_string.replaceAll(alp, capital)
+    // return answer
+    return my_string.replaceAll(alp, alp.toUpperCase())
 }
