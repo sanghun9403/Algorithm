7ec0d43a8d5fc29e304e048bce93ca13fdfd8194
sanghun9403 <132434240+sanghun9403@users.noreply.github.com>
2023-06-16 15:14:45 +0900

    [unrated] Title: 공배수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181936. 공배수/공배수.js
+++ 프로그래머스/unrated/181936. 공배수/공배수.js
@@ -1,3 +1,4 @@
 function solution(number, n, m) {
-    return number % n === 0 && number % m === 0 ? 1 : 0 
+    return number % n === 0 ? (number % m === 0) ? 1 : 0 : 0 
+    // return number % (n * m) === 0 ? 1 : 0 
 }
