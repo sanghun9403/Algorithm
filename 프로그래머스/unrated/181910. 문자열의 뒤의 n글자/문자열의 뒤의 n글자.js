c708ccaf6ae03015fe1265d63cc62afa49f56f76
sanghun9403 <132434240+sanghun9403@users.noreply.github.com>
2023-06-16 12:46:12 +0900

    [unrated] Title: 문자열의 뒤의 n글자, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
@@ -1,6 +1,3 @@
 function solution(my_string, n) {
-    let reverse = my_string.split("").reverse().join("")
-    let answer = reverse.substring(0, n).split("").reverse().join("")
-   
-    return String(answer)
+    return my_string.substring(my_string.length - n);
 }
